2e02f88a8004ad40b477e276bf8ef3ba37b2bcee
eyza <kratas.tom@seznam.cz>
2025-10-22 00:40:48 +0200

    Version 1.4.5.1-test.10

--- src/shared/version.h
+++ src/shared/version.h
@@ -3,4 +3,4 @@
 #define APP_VERSION_PROTOCOL 5 								
 #define APP_VERSION_PATCH 1 										
 #define APP_VERSION_IS_TEST 1 									
-#define APP_VERSION "1.4.5.1-test.9" 		
+#define APP_VERSION "1.4.5.1-test.10" 		
